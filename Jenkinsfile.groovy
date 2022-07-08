9ef2a69535e85674a82801bd33b19793e372a87a
Jin Mok <102392728+mokkang@users.noreply.github.com>
2022-07-07 17:42:01 -0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -115,14 +115,14 @@ pipeline {
                                     sh """
                                         curl -sO https://downloads.veracode.com/securityscan/pipeline-scan-LATEST.zip
                                         unzip pipeline-scan-LATEST.zip pipeline-scan.jar
-                                        java -jar veracode_scanner/pipeline-scan.jar --veracode_api_id $VERACODE_API_ID --veracode_api_key $VERACODE_API_KEY --file target/verademo.war --issue_details true -bf baseline_results.json
+                                        java -jar veracode_scanner/pipeline-scan.jar --veracode_api_id $VERACODE_API_ID --veracode_api_key $VERACODE_API_KEY --file app/target/verademo.war --issue_details true -bf baseline_results.json
                                         """
                                 }
                                 else {
                                     powershell """
                                             curl  https://downloads.veracode.com/securityscan/pipeline-scan-LATEST.zip -o pipeline-scan.zip
                                             Expand-Archive -Force -Path pipeline-scan.zip -DestinationPath veracode_scanner 
-                                            java -jar pipeline-scan.jar --veracode_api_id $VERACODE_API_ID --veracode_api_key $VERACODE_API_KEY --file target/verademo.war --issue_details true -bf powershell-baseline.json
+                                            java -jar pipeline-scan.jar --veracode_api_id $VERACODE_API_ID --veracode_api_key $VERACODE_API_KEY --file app/target/verademo.war --issue_details true -bf powershell-baseline.json
                                             """
                                 }
                             } catch (err) {
